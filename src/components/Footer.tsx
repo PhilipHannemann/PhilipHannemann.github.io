02d54fd20ebc458eb81f044712d7343f05c0d86f
PhilipHannemann <philip.hannemann@sjsu.edu>
2024-01-19 23:42:05 +0100

    add repo link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer({ extented }: FooterProps) {
         <p>
             This webside does not have the need for Next.js and React because there is not much interaction with its content and no server functionality is needed.<br />
             But to show, that I&apos;m good at developing I have made the entire website with Next.js.<br />
-            You can find the code of it here: <a className="text-orange" href="#" >Website source code <Github /></a>
+            You can find the code of it here: <a className="text-orange" href="https://github.com/PhilipHannemann/PhilipHannemann.github.io">Website source code <Github /></a>
         </p>
     )
 
