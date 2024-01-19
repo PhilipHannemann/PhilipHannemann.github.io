c75c46991d328499c886cc090c529be208e3b6fd
PhilipHannemann <philip.hannemann@sjsu.edu>
2024-01-19 22:23:52 +0100

    responsive navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -23,18 +23,28 @@ function NavLink({ children, href, className = "" }: NavLinkProps) {
     )
 }
 
-export default function NavBarV2() {
+export default function NavBar() {
+
+    const id = "navBar"
 
     return (
-        <div className="sticky-top top-0" style={{ zIndex: 1000 }}>
-            <nav className="container navbar navbar-expand py-3 text-uppercase">
-                <ul className="navbar-nav bg-blue-milk rounded-3 shadow w-100 p-2 position-relative fw-semibold">
-                    <NavLink href="/about">About Me</NavLink>
-                    <NavLink href="/projects">Projects</NavLink>
-                    <NavLink href="/resume">Resume</NavLink>
-                    <NavLink href="/contact">Contact</NavLink>
-                    <NavLink href="#" className="end-0 position-absolute px-2"><Github /></NavLink>
-                </ul>
+        <div className="sticky-top top-0 px-2 zindex-sticky">
+            <nav className="container navbar navbar-expand-sm py-3 text-uppercase">
+                <div className="container-fluid bg-blue-milk shadow rounded-3">
+                    <button className="navbar-toggler border-0 p-1 py-2" type="button" data-bs-toggle="collapse" data-bs-target={`#${id}`} aria-controls={id} aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id={id}>
+                        <ul className="navbar-nav p-2 position-relative fw-semibold">
+                            <NavLink href="/about">About Me</NavLink>
+                            <NavLink href="/projects">Projects</NavLink>
+                            <NavLink href="/resume">Resume</NavLink>
+                            <NavLink href="/contact">Contact</NavLink>
+
+                            <NavLink href="#" className="end-0 position-absolute px-2"><Github /></NavLink>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </div>
     );
