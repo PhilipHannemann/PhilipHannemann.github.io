344adf61c13b3b5f320d988a687825ac6e5a1727
PhilipHannemann <philip.hannemann@sjsu.edu>
2024-02-05 10:12:33 +0100

    update footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,20 +8,23 @@ export default function Footer({ extented }: FooterProps) {
   const headline = extented && <h3>One More Thing</h3>;
   const description = extented && (
     <p>
-      This webside does not have the need for Next.js and React because there is
-      not much interaction with its content and no server functionality is
-      needed.
+      While this website doesn&apos;t inherently require the use of Next.js and
+      React, I&apos;ve chosen to showcase my development skills by building the
+      entire site with Next.js. Despite the minimal interaction with content and
+      the absence of server functionality, this choice reflects my commitment
+      for creating cutting-edge technologies in my projects.
       <br />
-      But to show, that I&apos;m good at developing I have made the entire
-      website with Next.js.
       <br />
-      You can find the code of it here:{" "}
+      Curious about the code behind the scenes? Explore the website&apos;s
+      source code{" "}
       <a
         className="text-orange"
         href="https://github.com/PhilipHannemann/PhilipHannemann.github.io"
       >
-        Website source code <Github />
-      </a>
+        here&nbsp;
+        <Github />
+      </a>{" "}
+      to gain insights into the implementation.
     </p>
   );
 
