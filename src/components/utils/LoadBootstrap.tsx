55cdb23a55c39397ec2f66070baa74e629fa717e
PhilipHannemann <philip.hannemann@sjsu.edu>
2024-02-06 13:22:37 +0100

    Add documentation to the LoadBootstrap component

--- src/components/utils/LoadBootstrap.tsx
+++ src/components/utils/LoadBootstrap.tsx
@@ -5,6 +5,10 @@ import "@/styles/bootstrap.scss";
 
 import { useEffect } from "react";
 
+/**
+ * Loads the Bootstrap JavaScript bundle.
+ * This component should be placed at the root of the application.
+ */
 export default function LoadBootstrap() {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
