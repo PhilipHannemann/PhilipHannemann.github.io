016474b27f5ced94c16d99f6e3936a289a8527c2
PhilipHannemann <philip.hannemann@sjsu.edu>
2024-02-06 13:31:10 +0100

    move header to layout folder and add documentation

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,15 +1,24 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import SlideShow from "./SlideShow";
-import Type from "./Type";
+import SlideShow from "../SlideShow";
+import Type from "../Type";
 
 interface HeaderProps {
+  /** The title of the website. */
   title: string;
+  /** An array of image URLs for the slideshow. */
   slideShow?: ReadonlyArray<string>;
+  /** An array of strings to be typed sequentially. */
   typed?: ReadonlyArray<string>;
 }
 
+/**
+ * Represents the full screen image component at the start of the website.
+ * Contains a title and an optional typed subheadline.
+ * The image is chosen by the current url but can also be provided through a set
+ * of images presented as a slideshow with a fade transition.
+ */
 export default function Header({ title, slideShow, typed }: HeaderProps) {
   const path = usePathname();
 
