e679305f3870e32511f94da92c12b227c6334670
PhilipHannemann <philip.hannemann@sjsu.edu>
2024-02-06 13:17:51 +0100

    add documentation to video component

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,8 +1,14 @@
 interface VideoProps {
+  /** The source URL of the video. */
   src: string;
+
+  /** For optional CSS classes for the video component. */
   className?: string;
 }
 
+/**
+ * Renders a video component for MP4 content.
+ */
 export default function Video({ src, className }: VideoProps) {
   return (
     <video className={className} loop autoPlay>
