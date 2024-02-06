6f6290b81827617a3ed70f725b99a566fe402a1d
PhilipHannemann <philip.hannemann@sjsu.edu>
2024-02-06 13:20:06 +0100

    add documentation to the SkillSet component

--- src/components/SkillSet.tsx
+++ src/components/SkillSet.tsx
@@ -1,13 +1,26 @@
 import ProgressBar from "./ProgressBar";
 
+/** Represents a skill with its name and score. */
 export type Skill = [name: string, score: number];
 
 interface SkillSetProps {
+  /** An array of skills to be displayed on the left side. */
   leftSkills: ReadonlyArray<Skill>;
+
+  /** An array of skills to be displayed on the right side. */
   rightSkills: ReadonlyArray<Skill>;
 }
 
+/**
+ * A component that displays a set of skills with their scores using progress bars
+ * in a 2 column layout on large screens.
+ */
 export default function SkillSet({ leftSkills, rightSkills }: SkillSetProps) {
+  /**
+   * Converts an array of skills into an array of progress bars.
+   * @param skills - The array of skills to convert.
+   * @returns Array of progress bars.
+   */
   function skillsToView(skills: ReadonlyArray<Skill>) {
     return skills.map(([skill, score]) => (
       <ProgressBar key={skill} title={skill} percentage={score} />
