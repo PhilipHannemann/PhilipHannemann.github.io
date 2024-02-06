bd61293f7250f8acf5e3b9848d456a37620717e5
PhilipHannemann <philip.hannemann@sjsu.edu>
2024-02-06 13:17:03 +0100

    add documentation to link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -2,10 +2,16 @@ import NextLink from "next/link";
 import { ReactNode } from "react";
 
 interface LinkProps {
+  /** The URL to navigate to. */
   href: string;
+
+  /** The content of the link. */
   children: ReactNode;
 }
 
+/**
+ * Renders a link with an orange color on hover.
+ */
 export default function Link({ href, children }: LinkProps) {
   return (
     <NextLink href={href} className="link text-break">
