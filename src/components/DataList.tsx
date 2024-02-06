3308f94e821ce7b355e4bfc6b94dfd5d2552a9d3
PhilipHannemann <philip.hannemann@sjsu.edu>
2024-02-06 13:21:51 +0100

    Add documentation to the DataList component

--- src/components/DataList.tsx
+++ src/components/DataList.tsx
@@ -1,11 +1,16 @@
 import { FC, Fragment, ReactNode } from "react";
 
+/** The type of each element in the list. */
 export type ListElement = [Icon: FC, headline: string, content: ReactNode];
 
 interface DataListProps {
+  /** The list of elements to be displayed. */
   data: ReadonlyArray<ListElement>;
 }
 
+/**
+ * Renders a list of data with icons, a headline and some content next to it.
+ */
 export default function DataList({ data }: DataListProps) {
   const listView = data.map(([Icon, headline, content], index) => (
     <Fragment key={index}>
