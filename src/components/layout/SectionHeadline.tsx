9c97ec3c80bdcfa10bf50d120f001371053d4b92
PhilipHannemann <philip.hannemann@sjsu.edu>
2024-03-22 17:29:30 +0100

    enable section links

--- src/components/layout/SectionHeadline.tsx
+++ src/components/layout/SectionHeadline.tsx
@@ -1,30 +1,66 @@
-import { ReactNode } from "react";
-import { type Icon } from "react-bootstrap-icons";
+"use client";
+
+import { usePathname } from "next/navigation";
+import { CSSProperties, ReactNode, useState } from "react";
+import { type Icon as BIcon } from "react-bootstrap-icons";
+import Link from "../Link";
 
 interface SectionHeadlineProps {
   /** The optional icon to display left the headline. */
-  icon?: Icon;
-
+  Icon?: BIcon;
+  /** id for generation a reference link. */
+  linkId?: string;
   /** The headline content. */
   children: ReactNode;
 }
+
+const ICON_STYLE: CSSProperties = { left: "-40px" };
+const ICON_CLASSNAME = "position-absolute transiotion-opacity-05";
+
+const visibale = (show: boolean) => (show ? "opacity-1" : "opacity-0");
 
 /**
  * Renders a headline with an optional icon.
  */
 export default function SectionHeadline({
-  icon: TIcon,
+  Icon,
   children,
+  linkId,
 }: SectionHeadlineProps) {
-  const iconView = TIcon && (
-    <TIcon className="position-absolute" style={{ left: "-40px" }} />
+  const [linkVisible, setShowLink] = useState(false);
+  const path = usePathname();
+
+  const iconVisible = !linkId || !linkVisible;
+  const iconView = Icon && (
+    <Icon
+      className={`${ICON_CLASSNAME} ${visibale(iconVisible)}`}
+      style={ICON_STYLE}
+    />
   );
 
+  const linkIcon = linkId && (
+    <Link
+      href={`${path}#${linkId}`}
+      className={`${ICON_CLASSNAME} ${visibale(linkVisible)} pe-4`}
+      style={ICON_STYLE}
+    >
+      #
+    </Link>
+  );
+
+  const hideLink = () => setShowLink(false);
+  const showLink = () => setShowLink(true);
+
   return (
-    <div className="pb-4 text-primary">
-      <span className="position-relative h2">{iconView}</span>
-      <h2>{children}</h2>
+    <span className="py-4 text-primary">
+      <span onMouseEnter={showLink} onMouseLeave={hideLink}>
+        <span className="position-relative h2">
+          {iconView}
+          {linkIcon}
+        </span>
+        <h2 className="d-inline">{children}</h2>
+      </span>
       <hr className="text-action border border-1 border-top border-action opacity-100 w-60px" />
-    </div>
+    </span>
   );
 }
