5d290b0abff25ef927107993c37f5c49643374e8
PhilipHannemann <philip.hannemann@sjsu.edu>
2024-09-19 21:31:34 +0200

    make videos play inline on phones

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -11,7 +11,7 @@ interface VideoProps {
  */
 export default function Video({ src, className }: VideoProps) {
   return (
-    <video className={className} loop autoPlay>
+    <video className={className} loop autoPlay playsInline>
       <source src={src} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
